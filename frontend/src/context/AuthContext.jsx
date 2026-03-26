5e628f07ed929acaa3554eb710e33999491e8ac2
1CodeBreaker705 <ranjansingh0661@gmail.com>
2026-03-26 15:14:58 +0530

    fixed google login

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -34,6 +34,9 @@ export const AuthProvider = ({ children }) => {
     signInWithGoogle: async () => {
       await supabase.auth.signInWithOAuth({
         provider: 'google',
+        options: {
+          redirectTo: `${window.location.origin}`
+        }
       });
     },
     signInWithEmail: async (email, password) => {
